41cf8966eb0ebbe55d0596ade1a85b44db91adea
seo0jjjjj <seo0jjjjj@gmail.com>
2024-04-17 16:13:03 +0900

    feat: 테스트용 DataLoader추가

--- src/util/DataLoader.java
+++ src/util/DataLoader.java
@@ -1,13 +1,17 @@
 package util;
 
+import model.Card;
+import model.Product;
+
 import java.util.List;
 
 /**
  * 파일을 불러오고 저장하는 클래스
  */
 public interface DataLoader {
-    public List loadProductData();
+    public List<Product> loadProductData();
     public List loadRecipeData();
+    public List<Card> loadCardData();
     public List loadBucketData();
 
 }
--- src/util/MockDataLoader.java
+++ src/util/MockDataLoader.java
@@ -1,5 +1,6 @@
 package util;
 
+import model.Card;
 import model.Product;
 
 import java.util.ArrayList;
@@ -11,11 +12,18 @@ public class MockDataLoader implements DataLoader{
     @Override
     public List loadProductData() {
         ArrayList<Product> products = new ArrayList<>();
-        products.add(new Product())
+//        products.add(new Product())
     }
 
     @Override
     public List loadRecipeData() {
+    }
+
+    @Override
+    public List<Card> loadCardData() {
+        List<Card> cards = new ArrayList<>();
+
+        return cards;
     }
 
     @Override
