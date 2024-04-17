d7486a5596a146e46a205ceae8112c2c7a029ec1
seo0jjjjj <seo0jjjjj@gmail.com>
2024-04-17 17:57:47 +0900

    fix: ProductSerive 수정

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import service.DataService;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
@@ -7,5 +9,7 @@ public class Main {
         koKiosk.init(); // 키오스크 초기화
         koKiosk.start(); // 키오스크 실행
         koKiosk.end(); // 키오스크 종료
+
+        System.out.println( DataService.dataLoader.loadProductData());
     }
 }
