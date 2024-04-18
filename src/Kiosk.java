d8447680a51421228950a49491ba3719918055f2
seo0jjjjj <seo0jjjjj@gmail.com>
2024-04-18 17:59:10 +0900

    feat: 비지니스 로직 처리하는 Service 분리

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,39 +1,17 @@
-import GUI.ListTable;
-import model.Card;
-import model.Product;
-import model.Receipt;
-import util.DataLoader;
-
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Kiosk {
-    private final DataLoader dataLoader;
-    public static ArrayList<Card> cards;
-    public static ArrayList<Product> products;
-    public static ArrayList<Receipt> receipts;
-    public Scanner sc = new Scanner(System.in);
+    KioskService kioskService;
 
-    boolean debugMode = false; // 디버그 모드 설정
 
-    public Kiosk(DataLoader dataLoader, boolean debugMode){
-        this.dataLoader = dataLoader;
-        this.debugMode = debugMode;
+    public Kiosk(KioskService kioskService){
+        this.kioskService = kioskService;
     }
+
 
     /**
      * Kiosk 초기화 함수로 Kiosk 생성 시 호출되며 파일을 통해서 데이터를 불러옴.
      */
     public void init(){
-        cards = dataLoader.loadCardData();
-        products = dataLoader.loadProductData();
-        receipts = dataLoader.loadReceiptData();
-
-        if(debugMode){
-            new ListTable(cards);
-            new ListTable(products);
-            new ListTable(receipts);
-        }
+        kioskService.test();
     }
 
     /**
@@ -41,26 +19,6 @@ public class Kiosk {
      */
     public void start(){
 
-    }
-
-    /**
-     *
-     */
-    public void test() {
-        // 삭제해도돼요
-        while(debugMode) {
-            System.out.println("""
-                    테스트할 항목을 골라주세요.
-                    프로그램이 종료되어도 데이터 변화는 반영되지 않습니다.
-                    
-                    Product 값 변경
-                    1) Product의 N번째 값을 변경합니다.
-                    """);
-            int input = sc.nextInt();
-            Product product = products.get(input);
-            System.out.println("변경할 문자열 입력 :");
-            product.setName(sc.next());
-        }
     }
 
     /**
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
-import util.MockDataLoader;
-
 public class Main {
-    public static void main(String[] args) {
-        Kiosk koKiosk = new Kiosk(new MockDataLoader(), false);
 
+    public static void main(String[] args) {
+        Kiosk koKiosk = new Kiosk(new KioskService());
         koKiosk.init(); // 키오스크 초기화
 //        koKiosk.start(); // 키오스크 실행
-        koKiosk.test();
         koKiosk.end(); // 키오스크 종료
 
     }
