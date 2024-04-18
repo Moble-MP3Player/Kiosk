98b55b8837a18768537897a58cdf6f50baa39998
seo0jjjjj <seo0jjjjj@gmail.com>
2024-04-18 11:42:41 +0900

    feat: 테스트코드 추가 및 데이터 입력 추가

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,23 +1,59 @@
-import service.DataService;
+import model.Card;
+import model.Product;
+import model.Receipt;
+import util.DataLoader;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Kiosk {
+    private final DataLoader dataLoader;
+    public ArrayList<Card> cards;
+    public ArrayList<Product> products;
+    public ArrayList<Receipt> receipts;
+
+
+    public Kiosk(DataLoader dataLoader){
+        this.dataLoader = dataLoader;
+    }
+
     /**
      * Kiosk 초기화 함수로 Kiosk 생성 시 호출되며 파일을 통해서 데이터를 불러옴.
      */
     public void init(){
-
+        cards = dataLoader.loadCardData();
+        products = dataLoader.loadProductData();
+        receipts = dataLoader.loadReceiptData();
     }
 
     /**
      * 키오스크 실행 함수 화면 출력 및 기능 동작 관리
      */
     public void start(){
-
+        System.out.println(cards);
+        System.out.println(products);
+        System.out.println(receipts);
     }
+
+    /**
+     *
+     */
+    public void test() {
+        System.out.println("""
+            테스트할 항목을 골라주세요.
+            프로그램이 종료되어도 데이터 변화는 반영되지 않습니다.
+            """);
+        switch (new Scanner(System.in).nextInt()){
+
+        }
+    }
+
     /**
      * 키오스크 종료 함수, 저장되지 않은 변경된 파일을 저장하고 프로그램 종료
      */
-    public void end(){}
+    public void end(){
+
+    }
 
 
     /**
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-import service.DataService;
+import util.MockDataLoader;
 
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        Kiosk koKiosk = new Kiosk();
+        Kiosk koKiosk = new Kiosk(new MockDataLoader());
 
         koKiosk.init(); // 키오스크 초기화
-        koKiosk.start(); // 키오스크 실행
+//        koKiosk.start(); // 키오스크 실행
+        koKiosk.test();
         koKiosk.end(); // 키오스크 종료
 
-        System.out.println( DataService.dataLoader.loadProductData());
     }
 }
