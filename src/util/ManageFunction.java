b6706b88a98d28031b69e3ef913bdf71c2d20b2a
KanHyoeonJin <toa0709@gmail.com>
2024-04-19 13:13:05 +0900

    gitignore 수정

---
+++ src/util/ManageFunction.java
@@ -0,0 +1,23 @@
+package util;
+
+import model.Product;
+
+import java.util.ArrayList;
+
+public class ManageFunction {
+
+    ArrayList<Product> products=new MockDataLoader().loadProductData();
+
+
+    public ManageFunction(){
+
+    }
+
+    public static void main(String[] args) {
+        ManageFunction manageFunction=new ManageFunction();
+        System.out.println(manageFunction.products);
+        for(Product product: manageFunction.products){
+            System.out.println(product.getId()+product.getName()+product.getPrice()+product.getDate());
+        }
+    }
+}
