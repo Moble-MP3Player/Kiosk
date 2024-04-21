166bee05ac5f03dd0d2129f049756ffc945518b1
서윤오 <seo0jjjjj@gmail.com>
2024-04-21 18:58:32 +0900

    refactor: 필요없는 static 변수 삭제

--- src/kiosk/Kiosk.java
+++ src/kiosk/Kiosk.java
@@ -1,5 +1,6 @@
 package kiosk;
 
+import backend.db.DBs;
 import menu.CustomerService;
 import menu.ManagementService;
 
@@ -7,14 +8,14 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Kiosk {
-    public static boolean isContinue = true;
-    private Scanner sc = new Scanner(System.in);
+    private final Scanner sc = new Scanner(System.in);
     KioskService kioskService;
 
     /**
      * 키오스크를 생성하는데 초기화해야하는 변수들에게 초깃값을 부여합니다.
      */
     public void init() {
+        DBs.init();
         CustomerService customerService = new CustomerService(); // 고객 메뉴 생성
         ManagementService managementService = new ManagementService(); // 관리자 메뉴 생성
         kioskService = new KioskService(customerService, managementService); // 키오스크 서비스 생성
@@ -26,7 +27,7 @@ public class Kiosk {
      */
     public void start() throws InputMismatchException {
         System.out.println("\uD83D\uDE22 메뉴 디자인이 너무 구립니다.. 추천좀해주세요");
-        while (isContinue) {
+        while (true) {
             // 디자인 재능있으신분 도와주세요... 진짜 가독성 개별로에요
             System.out.println("====== [사용자모드] ======");
             System.out.println("메뉴를 선택해주세요.");
@@ -41,22 +42,14 @@ public class Kiosk {
             System.out.println();
             System.out.println();
 
-            int input = 0; // 입력 받기
-
-            try {
-                input = sc.nextInt();
-            }catch (InputMismatchException e){
-                System.out.println("입력이 정확하지 않습니다.");
-                input = -2;// 프로그램 안터지게 아무 숫자나 넣음.
-                sc.nextLine(); // 프로그램 안터지게 버퍼 비우기
-            }
+            int input = nextInt(); // 입력 받기
 
             if (input == 0) break; // 0입력시 종료
-            if (input != -1) kioskService.chooseAndExecuteUserMenu(input); // -1입력시 관리자 모드
+            if (input != -1) kioskService.chooseAndExecuteUserMenu(input); // 입력값이 정상일 때, 실행
             else {
                 // 관리자 모드
                 while (true) {
-                    System.out.println("===== [관리자 모드.] ======");
+                    System.out.println("===== [관리자 모드] ======");
                     System.out.println("메뉴를 선택해주세요.");
                     System.out.println("=======================");
 
@@ -66,14 +59,7 @@ public class Kiosk {
                     System.out.println("0) 관리자 모드 종료");
                     System.out.println("=======================");
 
-                    try {
-                        input = sc.nextInt();
-                    }catch (InputMismatchException e){
-                        System.out.println("입력이 정확하지 않습니다.");
-                        input = -2; // 프로그램 안터지게 아무 숫자나 넣음.
-                        sc.nextLine();
-                    }
-
+                    input = nextInt();
                     if (input == 0) break; // 0이면 종료
 
                     kioskService.chooseAndExecuteManagerMenu(input);
@@ -114,21 +100,21 @@ public class Kiosk {
      */
     public void end() {
         System.exit(0);
-    }
-
-
-    /**
-     * 관리자 모드로 전환하는 함수
-     */
-    public void enterManagerMode() {
-
+        sc.close();
     }
 
     /**
-     * 메뉴를 보여줍니다.
+     * 사용자로 부터 숫자를 입력받음.
+     * @return 입력 값이 부정확할 경우, -2 아닐 경우 입력받은 값
      */
-    public void showMenu() {
+    public int nextInt(){
+        int input = -2; // 프로그램 안터지게 아무 숫자나 넣음.
+        try{
+            input = sc.nextInt();
+        } catch (InputMismatchException e) {
+            sc.nextLine();
+            System.out.println("입력이 정확하지 않습니다.");
+        }
+        return input;
     }
-
-
 }
