c22806803499b027ce577fe8b54f425f0f1e0c84
서윤오 <seo0jjjjj@gmail.com>
2024-04-22 23:23:08 +0900

    feat: Scanner 오류 구문추가

--- src/kiosk/KioskService.java
+++ src/kiosk/KioskService.java
@@ -7,10 +7,10 @@ import menu.CustomerService;
 import menu.ManagementService;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 /**
  * 메뉴 전반 기능을 담당하는 클래스입니다.
- *
  */
 public class KioskService {
 
@@ -33,9 +33,10 @@ public class KioskService {
 
     /**
      * 해당 메뉴를 출력합니다.
+     *
      * @param menus 출력할 메뉴
      */
-    private void showMenu(ArrayList<KioskMenu> menus){
+    private void showMenu(ArrayList<KioskMenu> menus) {
         for (KioskMenu kioskMenu : menus) { // 메뉴 목록마다
             System.out.printf("%d) %s\n", // id) "menuTitle" 형식으로 출력합니다.
                     kioskMenu.getMenuId(),
@@ -54,17 +55,18 @@ public class KioskService {
     /**
      * 유저 메뉴를 보여줍니다.
      */
-    public void showUserMenu(){
+    public void showUserMenu() {
         showMenu(uesrMenus); // 관리자 메뉴 출력
     }
 
     /**
      * 해당 메뉴에 해당하는 매개변수로 입력받은 값으로 해당 메뉴의 함수를 실행시킵니다.
+     *
      * @param menuList KioskMenu 리스트
-     * @param input 사용자 입력
-     * @param obj 메뉴가 정의되어 있는 클래스의 객체
+     * @param input    사용자 입력
+     * @param obj      메뉴가 정의되어 있는 클래스의 객체
      */
-    private void chooseAndExecuteMenu(int input,ArrayList<KioskMenu> menuList,Object obj){
+    private void chooseAndExecuteMenu(int input, ArrayList<KioskMenu> menuList, Object obj) {
         boolean isExecuted = false;
 
         for (KioskMenu kioskMenu : menuList) {
@@ -72,33 +74,45 @@ public class KioskService {
                 try {
                     kioskMenu.getMenuMethod().invoke(obj, null);
                 } catch (Exception e) {
+                    DBs.setLogging(true);
+                    if (e.getCause().getClass() == InputMismatchException.class) throw new InputMismatchException("""
+                            콘솔로 입력받은 데이터의 자료형 불일치
+                            Scanner를 통한 입출력의 자료형이 맞지 않습니다.
+                            ex) sc.nextInt()를 통해 숫자 입력을 요청했는데 int가 아닌 String을 입력했을 경우
+
+                            해당 메뉴 제목 : %s
+                            해당 함수 이름 : %s << 여길 확인하세요.""".formatted(kioskMenu.getMenuTitle(), kioskMenu.getMenuMethod()));
+
+
                     DBs.log("reflection 오류 " + e.getCause());
                     throw new RuntimeException("서윤오한테 말좀해주세요..." + e.getCause());
+
                 }
                 isExecuted = true;
             }
         }
-        if(!isExecuted) {
+        if (!isExecuted) {
             System.out.println("입력 값이 정확하지 않습니다.");
         }
     }
 
 
-
     /**
      * 입력 값을 통해, 사용자 모드의 해당 번호에 해당하는 메뉴함수를 실행시킵니다.
+     *
      * @param input 사용자 입력
      */
-    public void chooseAndExecuteUserMenu(int input){ // 입력받은 input에 해당하는 유저메뉴 함수 출력
-        chooseAndExecuteMenu(input,uesrMenus,customerService);
+    public void chooseAndExecuteUserMenu(int input) { // 입력받은 input에 해당하는 유저메뉴 함수 출력
+        chooseAndExecuteMenu(input, uesrMenus, customerService);
     }
 
     /**
      * 입력 값을 통해, 관리자 모드의 해당 번호에 해당하는 메뉴함수를 실행시킵니다.
+     *
      * @param input 사용자 입력
      */
-    public void chooseAndExecuteManagerMenu(int input){  // 입력받은 input에 해당하는 관리자 메뉴 함수 출력
-        chooseAndExecuteMenu(input,managerMenus,managementService);
+    public void chooseAndExecuteManagerMenu(int input) {  // 입력받은 input에 해당하는 관리자 메뉴 함수 출력
+        chooseAndExecuteMenu(input, managerMenus, managementService);
     }
 
 }
